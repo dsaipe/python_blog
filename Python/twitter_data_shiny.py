1df1ab2123e37e809b3f2e26ae363b927e3417e6
dsaipe <d.f.saipe2@newcastle.ac.uk>
2022-08-04 16:46:22 +0100

    Updated table output

--- Python/twitter_data_shiny.py
+++ Python/twitter_data_shiny.py
@@ -6,22 +6,19 @@ Created on Mon Aug  1 11:55:28 2022
 """
 
 from shiny import App, ui, render
-import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
 import plotnine as gg
+import qgrid
 
 jr = pd.read_csv('Data/jr_shiny.csv')
-jr = jr.astype(object)
+jr = jr.astype({'year': 'object', 'day': 'object', 'hour': 'object'})
 
 #rstats = pd.read_csv('rstats_hashtag.csv')
 #rstats = rstats.filter(['screen_name', 'created_at', 'text', 'favorite_count',
 #                        'retweet_count'])
 
-choices = ({'year': 'Year',
-           'day': 'Day',
-           'hour': 'Hour',
-           'media_type': 'Media'})
+choices_select = ({'year': 'Year', 'day': 'Day', 'hour': 'Hour', 'media_type': 'Media'})
+choices_check = ({'created_at': 'Date', 'text': 'Text', 'retweet_count': 'Retweets', 'favorite_count': 'Likes'})
 
 app_ui = ui.page_fluid(
     ui.panel_title("@jumping_uk Twitter Data"),
@@ -29,16 +26,20 @@ app_ui = ui.page_fluid(
         ui.panel_sidebar(
             ui.input_select(id = "x",
                             label = "X-axis Variable",
-                            choices = {'year': 'Year',
-                                       'day': 'Day',
-                                       'hour': 'Hour',
-                                       'media_type': 'Media'},
+                            choices = choices_select, 
                             selected = 'year'),
-            ui.input_slider(id = "num",
-                             label = "How many tweets would you like to view?", 
+            ui.input_numeric(id = "num",
+                             label = "How many tweets do you want to view?", 
                              value = 0,
                              min = 0,
-                             max = 881)
+                             max = 50),
+            ui.panel_conditional(
+                "input.num > 0 && input.num <= 50",
+                ui.input_checkbox_group(id = "cols", 
+                                        label = "Select which variables you want to view:",
+                                        choices = choices_check,
+                                        selected = (['created_at', 'text']))
+                )
             ),
         ui.panel_main(
             ui.output_plot("bar"),
@@ -57,20 +58,21 @@ def server(input, output, session):
         avg_int = pd.melt(avg_int, id_vars = input.x())
         plot = (gg.ggplot(avg_int, gg.aes(input.x(), 'value', fill = 'variable')) +
                     gg.geom_col(position = 'dodge') +
-                    gg.xlab(str.title(choices[input.x()])) +
+                    gg.xlab(str.title(choices_select[input.x()])) +
                     gg.ylab("Average Interactions") +
                     gg.scale_fill_brewer(type = "qual", palette = "Dark2",
                                          name = "Interaction",
                                          labels = (["Favourite Count", "Retweet Count"])) +
                     gg.theme_classic())
         if input.x() == 'day':
-            plot = plot + gg.scale_x_discrete(labels = (["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]))
-        return plot
+            return plot + gg.scale_x_discrete(labels = (["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]))
+        else:
+            return plot
     @output
     @render.text
     def text():
-        if input.num() == 0:
-            return "Displaying no @jumping_uk tweets:"
+        if input.cols() == () or input.num() == 0 or input.num() < 0 or input.num() > 50:
+            return ""
         elif input.num() == 1:
             return "Displaying the most recent @jumping_uk tweet:"
         else:
@@ -78,14 +80,14 @@ def server(input, output, session):
     @output
     @render.table
     def table():
-        cols = jr.filter(['created_at', 'text', 'favorite_count', 'retweet_count'])
-        cols.rename(columns = {'created_at': 'Date', 'text': 'Text', 
-                               'favorite_count': 'Likes', 'retweet_count': 'Retweets'},
+        cols = jr.filter(input.cols())
+        cols.rename(columns = {'created_at': 'Date', 'text': 'Text', 'retweet_count': 'Retweets', 'favorite_count': 'Likes'}, 
                     inplace = True)
         pd.set_option('colheader_justify', 'left')
         first_n = cols.head(input.num())
-        return first_n
-    return server
+        if input.num() == 0 or input.num() < 0 or input.num() > 50:
+            return None
+        else:   
+            return first_n
     
-app = App(app_ui, server, debug = True)
-
+app = App(app_ui, server)
